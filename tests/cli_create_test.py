add36b03e2fccdad3e67815dfdd405496a42cb0d
Chris Mackey <chris@mackeyarchitecture.com>
2021-01-26 01:40:15 -0500

    feat(cli): Add command to generate L-shaped models
    * feat(cli): Add command for L-shaped model creation
    
    * feat(cli): Add command to generate L-shaped models

--- tests/cli_create_test.py
+++ tests/cli_create_test.py
@@ -5,7 +5,7 @@ from click.testing import CliRunner
 
 from honeybee.model import Model
 from honeybee.boundarycondition import Outdoors, Ground
-from honeybee.cli.create import shoe_box, rectangle_plan
+from honeybee.cli.create import shoe_box, rectangle_plan, l_shaped_plan
 
 
 def test_shoe_box_simple():
@@ -71,7 +71,7 @@ def test_rectangle_plan_detailed():
                for face in new_model.faces)
 
 
-def test_rectangle_plan_detailed():
+def test_rectangle_plan_custom():
     runner = CliRunner()
     in_arg = ['30', '20', '3.5', '-p', '5', '-a', '135', '-ar', '-af']
     result = runner.invoke(rectangle_plan, in_arg)
@@ -80,4 +80,16 @@ def test_rectangle_plan_detailed():
     new_model = Model.from_dict(model_dict)
     assert new_model.units == 'Meters'
     assert len(new_model.rooms) == 5
+    assert len(new_model.apertures) == 0
+
+
+def test_l_shaped_plan_detailed():
+    runner = CliRunner()
+    in_arg = ['20', '40', '15', '30', '3.5', '-p', '5', '-s', '3', '-a', '45']
+    result = runner.invoke(l_shaped_plan, in_arg)
+    assert result.exit_code == 0
+    model_dict = json.loads(result.output)
+    new_model = Model.from_dict(model_dict)
+    assert new_model.units == 'Meters'
+    assert len(new_model.rooms) == 21
     assert len(new_model.apertures) == 0
